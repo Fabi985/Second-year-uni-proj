021cfd8d696f7dfdd1faedab96c24d66e3c79ab0
Purple Rabbit <131457545+J-Purple-Rabit@users.noreply.github.com>
2026-02-20 14:19:40 +0000

    d

--- webAppProto/app/controllers/notFound.js
+++ webAppProto/app/controllers/notFound.js
@@ -2,5 +2,5 @@ import render from "../render.js";
 import { notFoundView } from "../views/notFound.js";
 
 export function notFoundController({ request }) {
-    return render(notFoundView({ request} ), {}, 404);
+    return render(notFoundView, { request });
 }
--- webAppProto/app/views/notFound.js
+++ webAppProto/app/views/notFound.js
@@ -1,10 +1,11 @@
 export function notFoundView({ request }) {
 
     const url = new URL(request.url);
+    const badPath = url.pathname
     return `
     <section aria-label="not found">
         <h2>Not found!</h2>
-        <p>Requested(${url}) resource does not exist.</p>
+        <p>Requested resource (${badPath} ) does not exist.</p>
     </section>
     `
 }
