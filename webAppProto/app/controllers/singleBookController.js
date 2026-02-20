fc2dd2863592bd410f61afc6f8ba7cc8e79e4e09
Fabi Velea <115619516+Fabi985@users.noreply.github.com>
2026-02-20 20:29:30 +0000

    switch em down nephew

--- webAppProto/app/controllers/singleBookController.js
+++ webAppProto/app/controllers/singleBookController.js
@@ -1,10 +1,13 @@
 import { getSpecificBook } from "../models/books.js";
+import { getChapterOfSpecificBook } from "../models/chapters.js";
 import render from "../render.js";
 import { singleBookView } from "../views/singleBookView.js";
 
 export function singleBookController({ BookId }) {
     const bookData = getSpecificBook(BookId);
+    const bookChapterData = getChapterOfSpecificBook(BookId);
+    console.log(bookChapterData);
     // console.log(bookData);
 
-return render(singleBookView, { bookData });
+    return render(singleBookView, { bookData, bookChapterData });
 }
