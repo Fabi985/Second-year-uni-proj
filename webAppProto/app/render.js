911d92833bfa6a89b023ae9e13899af279585815
Fabi Velea <115619516+Fabi985@users.noreply.github.com>
2026-02-20 12:41:48 +0000

    yipeee

---
+++ webAppProto/app/render.js
@@ -0,0 +1,34 @@
+export default function render(viewFn, data, status = 200) {
+    const content = viewFn(data);
+    const headers = new Headers();
+    headers.set("content-type", "text/html");
+    
+    const html = `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>App</title>
+                <meta charset="utf-8">
+                <link rel="icon" href="/assets/icon.svg">
+                <link rel="stylesheet" href="/assets/styles.css">
+            </head>
+            <body>
+                <header>
+                    <h1>web app</h1>
+                    <nav>
+                        <a href="/">Home</a>
+                        <a href="/items">about</a>
+                    </nav>
+                </header>
+                <main>
+                    ${content}
+                </main>
+                <footer>
+                    <p>&copy; :P</p>
+                </footer>
+            </body>
+        </html>
+        `
+
+    return new Response(html, { headers, status });
+}
---
+++ webAppProto/app/views/home.js
@@ -0,0 +1,9 @@
+export function homeView() {
+    return `
+    <section aria-labelledby="home-heading">
+        <h2 id="home-heading">Home Page</h2>
+        <p>This is the home page</p>
+        <p>This is for Slimballs</p>
+    </section>
+    `
+}
