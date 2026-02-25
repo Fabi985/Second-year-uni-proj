6ee5535b8627f9430a76f9b1f7939a8fb4af164e
Purple Rabbit <131457545+J-Purple-Rabit@users.noreply.github.com>
2026-02-25 14:31:55 +0000

    css

--- webAppProto/app/views/catalogue.js
+++ webAppProto/app/views/catalogue.js
@@ -1,28 +1,32 @@
 export function catalogueView({ books }) {
 
     const listBooks = books.map(book => `
-        <section id="book" aria-label="book">
-            <li>
+        <li>
+            <section id="book" aria-label="books">
                 <a href="/catalogue/book/${book.BookId}">
                     <img alt="${book.BookTitle} Cover" src="/assets/bookCovers/${book.BookImage}"/>
                     <p>${book.BookTitle}</p>
                 </a>
-            </li>
-        </section>
+            </section>
+        </li>
         `).join("\n");
 
     return `
     <section id="catalogue-page" aria-label="catalogue-page">
-    <h1>Catalogue:</h1>
+        <h1>Catalogue:</h1>
         <section id="book-search" aria-label="book-search">
             <form method="POST" action="/catalogue">
                 <input type="text" placeholder="Search book..." name="book-search" required>
                 <button type="submit">Search</button>
             </form>
         </section>
-        <ul>
-            ${listBooks}
-        </ul>
+    </section>
+    <section id="catalogue" aria-label="catalogue"
+        <section id="books-list" airia-label="books-list">
+            <ul>
+                ${listBooks}
+            </ul>
+        </section>
     </section>
     `
 }
